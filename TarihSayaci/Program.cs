33601f620c9bffa576e6cded108daafe9505f63a
Say My Name <Say My Name@DESKTOP-OKALL0L>
2023-04-12 01:14:55 +0300

    Proje dosyası ekle.

---
+++ TarihSayaci/Program.cs
@@ -0,0 +1,17 @@
+﻿DateTime tarih = new DateTime(2024, 1, 1);
+DateTime Bugun = DateTime.Now;
+
+double fark = tarih.Subtract(Bugun).Days;
+
+if (fark >= 1)
+{
+    Console.WriteLine("-----------------------");
+
+    Console.WriteLine($"{fark} gün kaldı");
+
+    Console.WriteLine("-----------------------");
+}
+else
+{
+    Console.WriteLine("Yarın!");
+}
